8e0ab3f5b9b35052ac7758a20d48c4d881d5c6cd
Darshan Sonare <darshansonare@example.com>
2025-07-30 20:23:47 +0530

    Updated frontend code

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'http://ev-charger-backend-production.up.railway.app',
   headers: {
     Authorization: localStorage.getItem('token')
   }
