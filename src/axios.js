02df382d45a255d5a5da7c0e15dede4bc404db19
Darshan Sonare <darshansonare@example.com>
2025-07-31 19:27:14 +0530

    all clean

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // uses the env variable
+  baseURL: 'http://localhost:3000', // uses the env variable
 });
 
 instance.interceptors.request.use(
